27ad72e3bd9bdc3b0d6f3e40b88ec78e56e0badb
KIM <wkdrn6246@naver.com>
2020-03-31 17:48:51 +0900

    init project

---
+++ src/main/java/cacao/friends/shop/CacaoFriendsShopApplication.java
@@ -0,0 +1,13 @@
+package cacao.friends.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacaoFriendsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacaoFriendsShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cacao/friends/shop/CacaoFriendsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package cacao.friends.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacaoFriendsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
