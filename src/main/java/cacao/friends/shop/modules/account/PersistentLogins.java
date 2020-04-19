c61ef0bd6cc848fd40acaa4bf558a950850a910f
KIM <wkdrn6246@naver.com>
2020-04-19 18:02:14 +0900

    rememberme login

---
+++ src/main/java/cacao/friends/shop/modules/account/PersistentLogins.java
@@ -0,0 +1,30 @@
+package cacao.friends.shop.modules.account;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity @Table(name = "persistent_logins")
+@Getter @Setter
+public class PersistentLogins {
+	
+	@Id
+	@Column(length = 64)
+	private String series;
+	
+	@Column(nullable = false, length = 64)
+	private String username;
+
+	@Column(nullable = false, length = 64)
+	private String token;
+
+	@Column(name = "last_used", nullable = false, length = 64)
+	private LocalDateTime lastUsed;
+
+}
